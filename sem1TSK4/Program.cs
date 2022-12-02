bebc59417ace90262430b26aa5b6420abd7ca8d5
Anna <anna@rumyantseva.org>
2022-12-02 14:25:11 +0300

    sem1TSK4 added

---
+++ sem1TSK4/Program.cs
@@ -0,0 +1,37 @@
+﻿string? inputNum1 = Console.ReadLine();
+string? inputNum2 = Console.ReadLine();
+string? inputNum3 = Console.ReadLine();
+
+int max=0;
+
+if (
+    inputNum1 != null &&
+    inputNum2 != null &&
+    inputNum3 != null
+    ) {
+    int num1 = int.Parse(inputNum1);
+    int num2 = int.Parse(inputNum2);
+    int num3 = int.Parse(inputNum3);
+
+    if (num1 > num2) {
+        max = num1;
+    } else {
+        max = num2;
+    }    
+
+    if (num3 > max)
+    {
+        max = num3;
+    }
+
+}
+
+Console.WriteLine($"Максимальное число: {max}");
+
+
+
+
+
+
+
+
