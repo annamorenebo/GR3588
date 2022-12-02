2cd991ee2f57160127f362137c070b3adea9201a
Anna <anna@rumyantseva.org>
2022-12-02 14:24:47 +0300

    sem1TSK3 added

---
+++ sem1TSK3/Program.cs
@@ -0,0 +1,12 @@
+﻿string? inputNum = Console.ReadLine();
+if(inputNum1!=null&&inputNum2!=null)
+{
+    int num1 = int.Parse(inputNum1);
+    int num2 = int.Parse(inputNum2);
+    int outResult = num1*num2;
+    Console.WriteLine(outResult);
+}
+
+
+
+
