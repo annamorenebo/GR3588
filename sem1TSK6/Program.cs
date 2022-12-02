7950d7ed5be3e563c91aa9d8879ba18dfcd48930
Anna <anna@rumyantseva.org>
2022-12-02 14:21:27 +0300

    sem1TSK6 added

---
+++ sem1TSK6/Program.cs
@@ -0,0 +1,20 @@
+﻿string? inputLine = Console.ReadLine();
+
+if(inputLine!=null)
+{
+    int num = int.Parse(inputLine);
+    int RemDiv=num%2;
+
+        if(RemDiv==0) 
+        {
+            Console.WriteLine("число четное");
+
+        }
+        else
+        {
+            Console.WriteLine("число нечетное");
+        }
+    
+}
+
+
