d9a57f1d46098acaffe47ac2845c8f8353a5bfbd
Anna <anna@rumyantseva.org>
2022-12-02 14:20:49 +0300

    sem1TSK8 added

---
+++ sem1TSK8/Program.cs
@@ -0,0 +1,22 @@
+﻿string? inputNum = Console.ReadLine();
+string? evenRow = String.Empty;
+if(inputNum!=null)
+{
+    int num = int.Parse(inputNum);
+    int evenNum=2;
+    while(evenNum<=num)
+    {
+        evenRow=evenRow+evenNum + ",";
+        evenNum=evenNum+2;
+    }
+
+ }
+
+Console.WriteLine( evenRow );
+
+
+
+
+
+
+
