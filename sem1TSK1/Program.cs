eed3189f88937e3765c0e8cf1d8b61ff3bab4e3a
Anna <anna@rumyantseva.org>
2022-12-02 14:22:22 +0300

    sem1TSK1 added

---
+++ sem1TSK1/Program.cs
@@ -0,0 +1,19 @@
+﻿string? inputNum1 = Console.ReadLine();
+string? inputNum2 = Console.ReadLine();
+
+if(inputNum1!=null&&inputNum2!=null)
+{
+    int num1 = int.Parse(inputNum1);
+    int num2 = int.Parse(inputNum2);
+        if(num2==num1*num1) 
+        {
+            Console.WriteLine("второе квадрат первого");
+
+        }
+        else
+        {
+            Console.WriteLine("второе не квадрат первого");
+        }
+    
+}
+
