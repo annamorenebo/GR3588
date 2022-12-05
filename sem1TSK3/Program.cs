131ca0c3edeb426baf678a51dc371be57d533da4
Anna <anna@rumyantseva.org>
2022-12-05 13:50:01 +0300

    sem1TSK3 added

--- sem1TSK3/Program.cs
+++ sem1TSK3/Program.cs
@@ -1,11 +1,17 @@
-﻿string? inputNum = Console.ReadLine();
-if(inputNum1!=null&&inputNum2!=null)
-{
-    int num1 = int.Parse(inputNum1);
-    int num2 = int.Parse(inputNum2);
-    int outResult = num1*num2;
-    Console.WriteLine(outResult);
-}
+﻿string? inputNum= Console.ReadLine();
+
+
+if(inputNum!=null)
+
+   { int num1 = int.Parse(inputNum);
+    string outDayOfWeek=string.Empty;
+    string outDayOfWeek=System.Globalization.CultureInfo.GetCultureInfo"ru-Ru".DateTimeFormat.GetDayName((DayOfWeek)Enum.GetValues(tipeof(DayOfWeek)).GetValue(inputNumber));
+
+   }
+        
+    
+    Console.WriteLine(outDayOfWeek);
+
 
 
 
