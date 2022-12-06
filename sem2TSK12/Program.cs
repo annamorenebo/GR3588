f9b1a8a735b196207507d245da21c191e1114c37
Anna <anna@rumyantseva.org>
2022-12-06 18:15:52 +0300

    sem2 TSK12 added

---
+++ sem2TSK12/Program.cs
@@ -0,0 +1,15 @@
+﻿int number1=int.Parse(Console.ReadLine()??"0");
+
+int number2=int.Parse(Console.ReadLine()??"0");
+int result = number2%number1;
+if (result==0)
+    {
+        Console.Write ("второе число кратно первому");
+
+    }
+else 
+    {
+        Console.Write ("второе число не кратно первому, остаток от деления равен"+ result);
+
+    }
+//Console.WriteLine(number1);
