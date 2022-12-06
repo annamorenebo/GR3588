125f108b812daa1035d72ba37b5e30a5fb9350bd
Anna <anna@rumyantseva.org>
2022-12-06 12:51:41 +0300

    sem1TSK11 added

---
+++ sem2TSK11/Program.cs
@@ -0,0 +1,14 @@
+﻿System.Random numberSintezator=new System.Random();
+int number =numberSintezator.Next(100,1000);
+Console.WriteLine(number);
+
+int firstNumber=number/100;
+int secondNumber=number%100/10;
+int thirdNumber=number%10;
+
+
+
+Console.WriteLine($"{firstNumber}{thirdNumber}");
+    
+
+
