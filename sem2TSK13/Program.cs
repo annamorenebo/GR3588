6384ac0c9731c5183e77b399f67474a140f7435b
Anna <anna@rumyantseva.org>
2022-12-06 20:52:02 +0300

    sem2TSK13 added

---
+++ sem2TSK13/Program.cs
@@ -0,0 +1,31 @@
+﻿int number=int.Parse(Console.ReadLine()??"0");
+
+
+int digit3=0;
+
+if(number>100)
+{
+    while(number/1000>0)
+
+    {
+        number=number/10;
+        
+    }
+digit3=number%10;
+Console.Write ($"третья цифра числа - "+ digit3);
+
+}
+else
+    {
+        Console.Write ($"третьей цифры нет ");
+    }
+    
+
+
+
+
+
+
+    
+
+
