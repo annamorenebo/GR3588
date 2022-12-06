0ba9b21fa1707fec499488ac8237cd48f3252d53
Anna <anna@rumyantseva.org>
2022-12-06 21:14:31 +0300

    sem2TSK15 added

---
+++ sem2TSK15/Program.cs
@@ -0,0 +1,25 @@
+﻿int number=int.Parse(Console.ReadLine()??"0");
+
+if(number>7)
+{
+    Console.Write ("Это не день недели, введите цифру от 1 до 7");
+
+} else if((number==6) || (number==7))
+{
+    Console.Write ("Да");
+
+} else
+{
+        Console.Write ("Нет ");
+}
+    
+
+
+
+
+
+
+    
+
+
+
