513e27edae913a00097bfd88e055e39768630229
Anna <anna@rumyantseva.org>
2022-12-06 12:32:49 +0300

    sem1TSK9 added

---
+++ sem2TSK9/Program.cs
@@ -0,0 +1,14 @@
+﻿System.Random numberSintezator=new System.Random();
+int number =numberSintezator.Next(10,100);
+Console.WriteLine(number);
+
+int firstNumber=number/10;
+int secondNumber=number%10;
+if(firstNumber>secondNumber)
+{
+    Console.WriteLine(firstNumber);
+    }
+else {
+    Console.WriteLine(secondNumber);
+
+}
