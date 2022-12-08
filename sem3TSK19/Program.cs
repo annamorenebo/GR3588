1e0f6d7e3b1a226dc695b334144e925b17e0ce44
Anna <anna@rumyantseva.org>
2022-12-08 19:43:06 +0300

    sem3TSK19, sem3TSK20 changed

--- sem3TSK19/Program.cs
+++ sem3TSK19/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("введите пятизначное число");
+string? strPalin = Console.ReadLine();
+
+bool res =((strPalin[0]==strPalin[4])&&(strPalin[1]==strPalin[3]));
+
+
+{
+    Console.WriteLine(res ? "Это  палиндром":"Это не палиндром");
+    
+}
--- sem3TSK20/Program.cs
+++ sem3TSK20/Program.cs
@@ -6,9 +6,9 @@ int ReadData(string msg)
 }
 
 // метод вычисляет расстояние
-double CalculateLenght(double x1, double y1, double x2, double y2)
+double CalculateLenght(double x1, double y1, double x2, double y2,)
 {
-    return Math.Sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+    return Math.Sqrt(((x1-x2)*(x1-x2))+((y1-y2)*(y1-y2)));
 }
 
 void PrintData(String msg, double res)
@@ -18,7 +18,10 @@ void PrintData(String msg, double res)
 
 int coordX1=ReadData("введите координаты точки x1");
 int coordY1=ReadData("введите координаты точки y1");
+
+
 int coordX2=ReadData("введите координаты точки x2");
 int coordY2=ReadData("введите координаты точки y2");
+
 
 PrintData("dist=", CalculateLenght(coordX1, coordY1, coordX2, coordY2 ));
