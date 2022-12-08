75c312b5c8626f96fc50fd9ca538dcf4f2e19882
Anna <anna@rumyantseva.org>
2022-12-08 19:17:16 +0300

    sem3TSK21,sem3TSK23 added

---
+++ sem3TSK21/Program.cs
@@ -0,0 +1,28 @@
+﻿//метод считывает введенные значение
+int ReadData(string msg)
+{
+    Console.Write(msg + ": ");
+    return int.Parse(Console.ReadLine()??"0");
+}
+
+// метод вычисляет расстояние
+double CalculateLenght(double x1, double y1, double x2, double y2, double z1, double z2)
+{
+    return Math.Sqrt(((x1-x2)*(x1-x2))+((y1-y2)*(y1-y2))+((z1-z2)*(z1-z2)));
+}
+
+void PrintData(String msg, double res)
+{
+    Console.WriteLine(msg + res.ToString("######.##"));
+}
+
+int coordX1=ReadData("введите координаты точки x1");
+int coordY1=ReadData("введите координаты точки y1");
+int coordZ1=ReadData("введите координаты точки z1");
+
+int coordX2=ReadData("введите координаты точки x2");
+int coordY2=ReadData("введите координаты точки y2");
+int coordZ2=ReadData("введите координаты точки z2");
+
+//  метод выводит значение дистанции
+PrintData("Расстояние =", CalculateLenght(coordX1, coordY1, coordX2, coordY2, coordZ1, coordZ2 ));
---
+++ sem3TSK23/Program.cs
@@ -0,0 +1,36 @@
+﻿// метод считывает данные от пользователя
+int ReadData(string msg)
+{
+    Console.Write(msg + ": ");
+    return int.Parse(Console.ReadLine()??"0");
+}
+//метод выстраивает таблицу
+string LineBilder(int N, int pow)
+{
+    string line = "|";
+
+    for(int i=1; i<=N; i++)
+    {
+        line += String.Format("{0,6} |", Math.Pow(i, pow));
+    }
+
+    return line;
+}
+// выстраивает горизонтальную линию
+string SepBuilder(int N)
+{
+    return "+" + new String('-', N * 8 - 1) + "+";
+}
+// метод выводит данные
+void PrintData(String msg, string res)
+{
+    Console.WriteLine(msg + res);
+}
+
+int numberN = ReadData("Введите число N");
+PrintData("", SepBuilder(numberN));
+PrintData("", LineBilder(numberN, 1));
+PrintData("", SepBuilder(numberN));
+PrintData("", LineBilder(numberN, 3));
+PrintData("", SepBuilder(numberN));
+
