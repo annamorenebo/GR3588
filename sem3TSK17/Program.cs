ae526493b12490ee3917c3ffca9c87c881bab4e4
Anna <anna@rumyantseva.org>
2022-12-08 15:42:15 +0300

    sem3TSK17,sem3TSK18 ,sem3TSK19 added

---
+++ sem3TSK17/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+//метод читает данные от пользователя
+int ReadData(string msg)
+
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine()??"0");
+}
+int coordX=ReadData("введите координату х");
+int coordY=ReadData("введите координату Y");
+
+// метод определяет четверть
+
+void PrintQuoterTest()
+{
+    if(coordX>0&&coordY>0) Console.WriteLine("quote 1");
+    if(coordX>0&&coordY<0) Console.WriteLine("quote 2");
+    if(coordX<0&&coordY<0) Console.WriteLine("quote 3");
+    if(coordX<0&&coordY>0) Console.WriteLine("quote 4");}
+
+PrintQuoterTest();
+
