b0c83d2182fda5af8c3504ec9e9051093d4b0ea8
Anna <anna@rumyantseva.org>
2022-12-12 18:43:45 +0300

    sem4TSK25 added

---
+++ sem4TSK25/Program.cs
@@ -0,0 +1,83 @@
+﻿//метод считывает данные от пользователя
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine()??"0");
+}
+//  считывает оператор
+string? ReadDataOper(string msg)
+{
+    Console.Write(msg);
+    
+    return Console.ReadLine();
+}
+
+// метод возводит число a в степень b
+int resPow(int a, int b)
+{
+    int res=1;
+    for(int i=1; i<=b; i++)
+    {
+        res=a*res;
+        
+    }
+return res;
+}
+
+
+//метод находит сумму чисел
+int resSum(int a, int b)
+{
+    return a+b;
+}
+
+// метод находит разность чисел
+int resDif(int a,int b)
+{
+    return a-b;
+}
+
+//метод находит произведение чисел
+int resProd(int a,int b)
+{
+    return a*b;
+}
+
+//метод находит частное чисел
+int resDiv(int a,int b)
+{
+    return a/b;
+}
+
+//метод выводит результат операции
+void PrintData(string msg, string res)
+{
+    Console.WriteLine(msg + res);
+}
+
+int a = ReadData("введите первое число:");
+string oper = ReadDataOper("введите операцию ( + , - , * , /  , P (для возведения в степень)");
+int b = ReadData("введите второе число:");
+
+
+
+if (oper[0] =='+')
+{
+    PrintData("result: ", resSum(a, b).ToString());
+}
+if (oper[0] == '-')
+{
+    PrintData("result: ", resDif(a, b).ToString());
+}
+if (oper[0] == '*')
+{
+    PrintData("result: ", resProd(a, b).ToString());
+}
+if (oper[0] == '/')
+{
+    PrintData("result: ", resDiv(a, b).ToString());
+}
+if (oper[0] == 'P')
+{
+    PrintData("result: ", resPow(a, b).ToString());
+}
